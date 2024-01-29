796f5165a7aaf2dd70f8f5ce51282733d3e76a4a
reetika <reetika.singh05@gmail.com>
2024-01-29 16:10:07 +0530

    added about section

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Layout from "../../Components/Layouts/Layout";
 import Section1 from "./Section1";
+import Section2 from "./Section2";
 import "../../styles/HomeStyle.css";
 
 function Home() {
   return (
     <Layout>
       <Section1 />
+      <Section2 />
       {/* <div>Home!!</div> */}
     </Layout>
   );
